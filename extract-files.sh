df787e0b70ccaf2ed178a9d0ca0a8830e1d5d683
LuK1337 <priv.luk@gmail.com>
2022-10-15 23:05:18 +0200

    sunny: Prevent adding same dependency twice in blob_fixup
    Change-Id: Idedeb51e027a09f64a29ac67a3a2f2a0594213f6
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -56,12 +56,12 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/camera/components/com.qti.node.watermark.so)
-            ${PATCHELF} --add-needed "libpiex_shim.so" "${2}"
+            grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
         vendor/lib64/hw/camera.qcom.so | vendor/lib64/libFaceDetectpp-0.5.2.so | vendor/lib64/libfacedet.so)
-            sed -i "s|libmegface.so|libfacedet.so|g" "${2}"
-            sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
-            sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
+            grep -q "libfacedet.so" "${2}" || sed -i "s|libmegface.so|libfacedet.so|g" "${2}"
+            grep -q "libFaceDetectpp-0.5.2.so" "${2}" || sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
+            grep -q "facedetectpp_0_5_2_model" "${2}" || sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
             ;;
         vendor/lib64/android.hardware.camera.provider@2.4-legacy.so)
             grep -q "libcamera_provider_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_provider_shim.so" "${2}"
