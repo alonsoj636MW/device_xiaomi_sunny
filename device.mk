b5e6c7ab91ca2d72388a6f4962bd390d2bcbc240
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-04 20:36:50 +0530

    sunny: Switch to AIDL Wi-Fi
    - This should fix Wi-Fi on A14.
    
    Change-Id: I2a0af66b664123fdb5de0dc5d222a4ecb0ae7892
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -530,7 +530,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     ipacm \
     IPACM_cfg.xml \
