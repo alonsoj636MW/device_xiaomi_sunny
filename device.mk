2722d1e3351981c7a7df2322269f14c0f80096b1
Sarthak Roy <sarthakroy2002@gmail.com>
2023-11-04 13:11:21 +0530

    sunny: Move to Android.bp
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Id71254de8c516c610eec3a967e394392efd28944
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -419,6 +419,7 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
     fstab.default \
+    fstab.default.ramdisk \
     init.qcom.rc \
     init.recovery.qcom.rc \
     init.target.rc \
