8ecef2f1064b34f510f36aefc510c8a0e5d5acda
Edwin Moquete <edwinmmoquete@gmail.com>
2024-03-16 01:24:14 +0000

    sunny: Build android.media.audio.common.types-V2-cpp
    Needed for WFD, and not built default on QPR2
    
    Change-Id: If860613ef4cb6bae7f7a7e72707c24510e36abd6
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -549,6 +549,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp \
     libavservices_minijail_32 \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
