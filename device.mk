8b98b981fc351a6cec891fae6d64fe38996205ad
alonsoj636 <alonsojimenez.jm3@gmail.com>
2024-04-08 12:46:58 -0400

    sunny: Inherit GApps config
    Signed-off-by: alonsoj636 <alonsojimenez.jm3@gmail.com>

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, vendor/xiaomi/sunny/sunny-vendor.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# GApps
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+
 # A/B
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
