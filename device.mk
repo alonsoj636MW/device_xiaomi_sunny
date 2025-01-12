a7c7fed3f3795d60aebfce8fde3a63f339660fa2
Cyber Knight <cyberknight755@gmail.com>
2025-01-12 18:57:38 +0800

    sunny: Don't add hardware/xiaomi to namespace
    - All dependencies have been imported to the tree.
    
    Change-Id: Ifea7f0ddf71b78c25c40e4daafcebb51979a87f9
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -435,8 +435,7 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    hardware/xiaomi
+    $(LOCAL_PATH)
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
