a2bbf52a3ebc90d84e372ab9477b395d2ceecffb
basamaryan <basam.aryan@gmail.com>
2025-04-02 16:12:24 +0200

    sunny: Bring back hardware/xiaomi
    - Also inherit hardware/xiaomi common VINTF fragment.
    
    Change-Id: I16f610364ea265d2a55460e345eebc6dcde1758d
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -434,7 +434,8 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/xiaomi
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
